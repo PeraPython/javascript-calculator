86f29eebcc0c63c9d86166d87a1dbd9dfe873e63
Marko <marko.perendic@yahoo.com>
2019-02-28 12:07:11 +0100

    Updates

---
+++ precache-manifest.c15804520d8a0f88f699a1c9e87449c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c5b888724864287a5b5",
+    "url": "/javascript-calculator/static/js/runtime~main.8c5b8887.js"
+  },
+  {
+    "revision": "1f672c70508bfecccebf",
+    "url": "/javascript-calculator/static/js/main.1f672c70.chunk.js"
+  },
+  {
+    "revision": "a3d03dde9f1fff44daee",
+    "url": "/javascript-calculator/static/js/2.a3d03dde.chunk.js"
+  },
+  {
+    "revision": "1f672c70508bfecccebf",
+    "url": "/javascript-calculator/static/css/main.2cdd7099.chunk.css"
+  },
+  {
+    "revision": "66d1ea93b7d4f60ab6756655c9608b97",
+    "url": "/javascript-calculator/index.html"
+  }
+];
